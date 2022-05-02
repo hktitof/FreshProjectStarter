963f85d6105dae7c1c248a6be1659ecf15509c11
titof <hktitof@gmail.com>
2022-05-02 18:16:18 +0000

    set up typescript first

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  
+  return (
+    <>
+      <div>Hello word! By Titof</div>
+      <p>current Project is set up, Next js,React js, Tailwind css and Typescript</p>  
+    </>
+  )
+}
