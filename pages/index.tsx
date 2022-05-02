551526b9f2eeb2aa7fb5c52fb1383c4f6da3f266
titof <hktitof@gmail.com>
2022-05-02 19:38:03 +0000

    adding text to index by mentining storybook and webpack is added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div>Hello word! By Titof</div>
-      <p>current Project is set up, Next js,React js, Tailwind css and Typescript</p>  
+      <p>current Project is set up with Next js,React js,Typescript, Tailwind css, storybook and webpack</p>  
     </>
   )
 }
